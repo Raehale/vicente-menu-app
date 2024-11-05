44ed70769b903ea9aee17b4078a91eac07a13885
vrhernandez <vicenterh99@gmail.com>
2024-11-04 22:32:49 -0800

    added order section and styling, and ability to add items to order

--- index.js
+++ index.js
@@ -1,21 +1,55 @@
 import { menuArray } from "./data.js";
 
-const menu = document.getElementById("menu")
+const menuEl = document.getElementById("menu-el")
+const orderEl = document.getElementById("order-el")
+const orderArray = []
 
+renderMenu()
 
-render()
+menuEl.addEventListener("click", function(e){
+    const targetEl = e.target
+    if (targetEl.classList.contains("add-item-btn")){
+        addItemToOrder(targetEl.dataset.id)
+    }
+    else if (targetEl.parentElement.classList.contains("add-item-btn")){
+        addItemToOrder(targetEl.parentElement.dataset.id)
+    }
+    renderOrder()
+})
 
-function render() {
-    menu.innerHTML = menuArray.reduce(function(innerHTML, currentItem){
+function addItemToOrder(itemId){
+    if (orderArray.length === 0){
+        orderEl.parentElement.classList.remove("hidden")
+    }
+    orderArray.push(menuArray[itemId])
+}
+
+function renderOrder(){
+    orderEl.innerHTML = orderArray.reduce(function(innerHTML, currentItem){
+        const {name, price} = currentItem
+        return innerHTML + `
+            <div class="order-line">
+                    <p class="big-text">${name}</p>
+                    <button class="remove-item-btn">remove</button>
+                    <p class="item-price align-right">$${price}</p>
+            </div>
+        `
+    },"")
+}
+
+function renderMenu(){
+    menuEl.innerHTML = menuArray.reduce(function(innerHTML, currentItem){
         const {name, ingredients, id, price, emoji} = currentItem
         return innerHTML + `<div class="menu-item">
                     <p class="item-graphic">${emoji}</p>
                     <div>
-                        <h3 class="item-name">${name}</h3>
+                        <p class="big-text">${name}</p>
                         <p class="item-ingredients">${ingredients.join(", ")}</p>
                         <p class="item-price">$${price}</p>
                     </div>
-                    <button class="add-item-btn" data-id="${id}"><i class="fa-regular fa-plus"></i></button>
+                    <button class="add-item-btn align-right" data-id="${id}">
+                        <i class="fa-regular fa-plus"></i>
+                    </button>
                 </div>`
     },"")
 }
